ec130a70fee6938136c892f60a133ccfa6141a35
JaeminKim <britten714@naver.com>
2017-12-10 21:28:47 +0900

    Made a Health Bar
    Q. Why the rest half of the Health Bar not shown on screen?

---
+++ Assets/Camera & UI/PlayerHealthBar.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(RawImage))]
+public class PlayerHealthBar : MonoBehaviour
+{
+
+    RawImage healthBarRawImage;
+    Player player;
+
+    // Use this for initialization
+    void Start()
+    {
+        player = FindObjectOfType<Player>();
+        healthBarRawImage = GetComponent<RawImage>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xValue = -(player.healthAsPercentage / 2f) - 0.5f;
+        //float xValue = -(player.healthAsPercentage / 2f) + 1.5f;  내가 구한 계산식. 위나 이거나 같다. 단지 이걸로 하면 게임 시작 시 UV Rect X값이 1에서 시작함.  
+        healthBarRawImage.uvRect = new Rect(xValue, 0f, 0.5f, 1f);
+    }
+}
