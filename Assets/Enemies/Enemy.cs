ae2a13b2e369efe4d87af2bc0f42b2fa6340684a
JaeminKim <britten714@naver.com>
2017-12-10 21:48:41 +0900

    Adding Enemy Health Bar
    Have some problem about enemy health bar shifting and floating, but will fix it later (lecture 41)

---
+++ Assets/Enemies/Enemy.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    [SerializeField] private float maxHealthPoints = 100f;
+    private float currentHealthPoints = 100f;
+
+    public float healthAsPercentage
+    {
+        get { return currentHealthPoints / maxHealthPoints; }
+    }
+}
